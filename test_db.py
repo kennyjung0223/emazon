7ed7967095790922eb260c189600341b313cbaca
jluna2000 <jesseluna2000@gmail.com>
2021-12-10 14:29:20 -0800

    further testing

--- test_db.py
+++ test_db.py
@@ -32,30 +32,60 @@ print("------------------- ORDER DETAILS -------------------")
 for order_detail in order_details:
     print(order_detail.id, order_detail.order_id, order_detail.product.name, order_detail.count)
 
-productName = 'iPhone 13 Pro'
-product = Product.query.filter_by(name = productName).first()
-print(product)
-print(product.id, product.name, product.description, product.price, product.picture, product.brand, product.category, product.stock_count)
+# productName = 'iPhone 13 Pro'
+# product = Product.query.filter_by(name = productName).first()
+# print(product)
+# print(product.id, product.name, product.description, product.price, product.picture, product.brand, product.category, product.stock_count)
+# productInfoDic = {
+# 	'name':product.name,
+# 	'description':product.description,
+# 	'price':product.price,
+# 	'picture':product.picture,
+# 	'brand':product.brand,
+# 	'category':product.category,
+# 	'stock count':product.stock_count
+# }
+# print(productInfoDic)
+# productInfoDicJson = json.dumps(productInfoDic)
+# print(productInfoDicJson)
 
-allProducts = Product.query.all()
-listOfAllProductDics = []
-for i in allProducts:
-	print(i.id, i.name, i.description, i.price, i.picture, i.brand, i.category, i.stock_count)
-	temp = {
-		'name':i.name,
-		'description':i.description,
-		'price':i.price,
-		'picture':i.picture,
-		'brand':i.brand,
-		'category':i.category,
-		'stock count':i.stock_count
-	}
-	listOfAllProductDics.append(temp)
+# print("-------------------------------------------------------------\n\n\n")
 
-listOfAllProductDicsJson = []
-for i in range(len(allProducts)):
-	print(listOfAllProductDics[i])
-	listOfAllProductDicsJson.append(json.dumps(listOfAllProductDics[i]))
+# allProducts = Product.query.all()
+# listOfAllProductDics = []
+# for i in allProducts:
+# 	print(i.id, i.name, i.description, i.price, i.picture, i.brand, i.category, i.stock_count)
+# 	temp = {
+# 		'name':i.name,
+# 		'description':i.description,
+# 		'price':i.price,
+# 		'picture':i.picture,
+# 		'brand':i.brand,
+# 		'category':i.category,
+# 		'stock count':i.stock_count
+# 	}
+# 	listOfAllProductDics.append(temp)
 
-for i in range(len(allProducts)):
-	print(listOfAllProductDicsJson[i])
+# listOfAllProductDicsJson = []
+# for i in range(len(allProducts)):
+# 	print(listOfAllProductDics[i])
+# 	listOfAllProductDicsJson.append(json.dumps(listOfAllProductDics[i]))
+
+# print("\n\nJSON versions")
+
+# for i in range(len(allProducts)):
+# 	print(listOfAllProductDicsJson[i])
+
+lebronUsername = 'ljames'
+lebron = User.query.filter_by(username = lebronUsername).first()
+print("Name: " + lebron.name)
+print("ID: " + str(lebron.id))
+
+orderForLebron = Order.query.filter_by(user_id = lebron.id).first()
+print(orderForLebron)
+# print(orderForLebron.id, orderForLebron.user_id, orderForLebron.user.username, orderForLebron.address_id, orderForLebron.address.street, orderForLebron.date, orderForLebron.subtotal, orderForLebron.tax, orderForLebron.shipping_fee)
+
+orderDetailsForLebron = OrderDetail.query.filter_by(order_id = orderForLebron.id).all()
+print(orderDetailsForLebron)
+for orderProducts in orderDetailsForLebron:
+	print("Product: " + orderProducts.product.name + " Count: " + str(order_detail.count))
